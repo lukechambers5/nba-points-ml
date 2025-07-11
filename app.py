85283cfedc99f8f250b04ea07f6b18489a8f2ebe
Luke Chambers <lukechambers0217@gmail.com>
2025-07-11 09:08:18 -0500

    Better suited for fly.io

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def predict():
     return render_template('index.html', prediction=result, team=opponent)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
